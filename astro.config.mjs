d1647fbfffe72d13d349deddc3241af6686dbef7
kebug666 <kebug666@gmail.com>
2025-10-20 22:52:20 +0800

    add github pages action

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://bug521.github.io",
+	base: "/astro-blog-fuwari",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
